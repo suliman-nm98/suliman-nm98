4c209a9c215db93b95d2482d4eebabeb17ae09d0
suliman <orzavon@gmail.com>
2020-10-03 21:38:11 +0400

    add diagonal gradient to page

---
+++ src/components/header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <p>Me</p>
+      <div className="header__logo">
+        <table>
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+      <p>You</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__section home__banner">
+        <h1>Suliman Noor</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
