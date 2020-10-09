8d14c9d28161455072371c6e701ef7845211a0c5
suliman <orzavon@gmail.com>
2020-10-09 18:01:50 +0400

    add right timeline item

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -4,10 +4,31 @@ function Timeline() {
   return (
     <div className="timeline">
       <h1>Timeline</h1>
-      <div className="timeline__left timeline__item">
+      <div className="timeline__itemLeft timeline__item">
         <h5 className="timeline__title">Web Developer</h5>
-        <p>helping a company to overhaul their website </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
       </div>
+      <div className="timeline__itemRight timeline__item">
+        <h5 className="timeline__title">Web Developer</h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
       <ul className="timeline__list">
         {/* <!-- Item 1 --> */}
         <li>
