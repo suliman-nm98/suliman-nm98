d8ca0f68701389b26ac4d4cc6666387dfb365bd0
suliman <orzavon@gmail.com>
2021-01-07 16:29:08 +0400

    Updates

---
+++ precache-manifest.c75b2647bf0d60aab3fdad560b772935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822123225bf2c276751b5fe9fc7541eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123160ea72468f16ed48",
+    "url": "/static/css/main.f7ab2eb6.chunk.css"
+  },
+  {
+    "revision": "ff29a53d870a468b2851",
+    "url": "/static/js/2.d07e56eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d07e56eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123160ea72468f16ed48",
+    "url": "/static/js/main.a3656275.chunk.js"
+  },
+  {
+    "revision": "9ac23bcbee4e1619d2af",
+    "url": "/static/js/runtime-main.cf710811.js"
+  }
+]);
