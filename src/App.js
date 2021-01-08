82097a08fda9aa3d895d15a4633d6abef1c52852
suliman <orzavon@gmail.com>
2021-01-08 15:06:29 +0400

    add links to social medai

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/home/Home";
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <Home />
     </div>
   );
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import "./Banner.css";
+
 function Banner() {
   return (
     <div className="banner">
@@ -8,10 +16,29 @@ function Banner() {
           src="https://avatars0.githubusercontent.com/u/34018201?s=400&u=e61f7958d14d15ab64aed18c576929d636cb75a6&v=4"
           alt=""
         />
-        <h1>SULIMAN NOOR</h1>
-        <h3>Developer, Designer, Athletic</h3>
-        <div className="socialmedia__links">
-          <a href="null">{/* <img src="_"> */}</a>
+        <h1>Hi, I'm Suliman🤝</h1>
+        <h3>Developer, Designer & Athletic</h3>
+        <div className="socialMedia__links">
+          <a href="www.linkedin.com/in/suliman-nm98" target="_blank">
+            <span>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </span>
+          </a>
+          <a href="www.linkedin.com/in/suliman-nm98" target="_blank">
+            <span>
+              <FontAwesomeIcon icon={faInstagram} />
+            </span>
+          </a>
+          <a href="www.linkedin.com/in/suliman-nm98" target="_blank">
+            <span>
+              <FontAwesomeIcon icon={faGithub} />
+            </span>
+          </a>
+          <a href="www.linkedin.com/in/suliman-nm98" target="_blank">
+            <span>
+              <FontAwesomeIcon icon={faTwitter} />
+            </span>
+          </a>
         </div>
       </div>
     </div>
